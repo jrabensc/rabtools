662a86e850e5f304012338900fcdd47ea422c99c
Johannes Rabenschlag <johannes.rabenschlag@posteo.de>
2021-07-22 21:16:04 +0200

    created namespace file and exported respective functions

--- R/env_default.R
+++ R/env_default.R
@@ -6,7 +6,8 @@
 #' @return Default value or content of environment variable
 #'
 #' @examples
-#' locale <- env_default(env = "locale", default = "e_DE.utf8")
+#' locale <- env_default(env = "locale", default = "de_DE.utf8")
+#' @export
 env_default <- function(env, default) {
   var <- base::ifelse(base::Sys.getenv(env) == "", default, base::Sys.getenv(env))
   base::return(var)
