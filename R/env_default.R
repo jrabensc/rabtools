7a607c3de271dfbc2153ea9e878e35676b9d486f
Johannes Rabenschlag <johannes.rabenschlag@posteo.de>
2021-07-22 20:48:20 +0200

    first commit

---
+++ R/env_default.R
@@ -0,0 +1,20 @@
+#' Use default value if environment variable is empty
+#'
+#' @param env Name of the environment variable as string
+#' @param default Default value to use when environment variable is empty
+#'
+#' @return Default value or content of environment variable
+#'
+#' @examples
+#' locale <- env_default(env = "locale", default = "e_DE.utf8")
+env_default <- function(env, default) {
+  var <- base::ifelse(base::Sys.getenv(env) == "", default, base::Sys.getenv(env))
+  base::return(var)
+}
+
+
+
+
+
+
+
